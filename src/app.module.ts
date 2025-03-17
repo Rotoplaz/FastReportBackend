6cf9800e201264ee92abfcceec31e9438a0c5cfd
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2025-03-17 00:29:51 -0600

    WIP: crud users, handle errors, prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EndpointExmpleModule } from './endpoint-exmple/endpoint-exmple.module';
 import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [EndpointExmpleModule, UsersModule],
+  imports: [EndpointExmpleModule, UsersModule, PrismaModule],
   controllers: [],
   providers: [],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,34 +1,99 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, ConflictException, InternalServerErrorException, BadRequestException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
 import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class UsersService {
+  constructor(private prisma: PrismaService) {}
 
-  create(createUserDto: CreateUserDto) {
-    const hashPasword =  bcrypt.hashSync(createUserDto.password, 10);
+  async create(createUserDto: CreateUserDto) {
+    const hashPasword = bcrypt.hashSync(createUserDto.password, 10);
+    try {
+      const { password, ...newUser } = await this.prisma.user.create({
+        data: {
+          ...createUserDto,
+          password: hashPasword,
+        },
+      });
 
-    return {
-      ...createUserDto,
-      password:hashPasword
+      return newUser;
+    } catch (error) {
+      this.handleErrors(error)
     }
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    const users = await this.prisma.user.findMany({
+      omit: { password: true },
+    });
+
+    return users;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+      omit: { password: true },
+    });
+
+    if (!user) {
+      throw new NotFoundException(`Usuario con ID ${id} no encontrado`);
+    }
+
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    try {
+      const updatedUser = await this.prisma.user.update({
+        where: { id },
+        data: updateUserDto,
+      });
+
+      return updatedUser;
+    } catch (error) {
+      this.handleErrors(error,  id);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: string) {
+    try {
+      await this.prisma.user.delete({
+        where: { id },
+      });
+
+      return { message: `Usuario con ID ${id} eliminado correctamente` };
+    } catch (error) {
+
+      this.handleErrors(error,  id);
+    }
+  }
+
+  handleErrors(error: any, id?: string){
+
+    if(!(error instanceof PrismaClientKnownRequestError)) {
+      if (error.message.includes('Argument `data` is missing.')) {
+        throw new BadRequestException('El cuerpo de la solicitud no puede estar vacío');
+      }
+    }
+
+    switch(error.code) {
+      case 'P2002': 
+        const target = error.meta?.target as string[];
+
+        if (target.includes('code')) {
+          throw new ConflictException('El código ya está en uso');
+        } else if (target.includes('email')) {
+          throw new ConflictException('El correo electrónico ya está en uso');
+        }
+      case 'P2025': throw new NotFoundException(`Usuario con ID ${id} no encontrado`);
+
+      default: throw new InternalServerErrorException();
+
+    }
+    
   }
 }
