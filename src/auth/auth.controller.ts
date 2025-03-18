0f7636eafdecc352f46b9e94421f63148a6f1a15
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2025-03-17 21:27:16 -0600

    register student

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { RegsiterStudentDto } from './dto/register-student.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,6 +10,11 @@ export class AuthController {
   @Post('login')
   login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto)
+  }
+
+  @Post('register')
+  registerStudent(@Body() regsiterStudentDto: RegsiterStudentDto) {
+    return this.authService.registerStudent(regsiterStudentDto)
   }
 
   
