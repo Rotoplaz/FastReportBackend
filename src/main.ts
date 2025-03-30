ddb56a1d9e8d7af308e15b609afa95257b05c9ee
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2025-03-29 20:13:54 -0600

    install swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,18 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     })
   );
+
+
+  const config = new DocumentBuilder()
+  .setTitle('Fast Report API')
+  .setDescription('This is a Fast Report API ')
+  .setVersion('1.0')
+  .addTag('Report App Backend')
+  .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
